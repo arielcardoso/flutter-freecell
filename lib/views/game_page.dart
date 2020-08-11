0ca6f1825f21afa9c39053abfa5cedbe87e061fd
Ariel Cardoso <arielvix@gmail.com>
2020-08-11 02:16:56 -0700

    Update main class

---
+++ lib/views/game_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'column.dart';
+
+class GamePage extends StatefulWidget {
+  GamePage({Key key}) : super(key: key);
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+
+  int userScore = 0;
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final SnackBar snackBar = const SnackBar(content: Text('Showing Snackbar'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Freecell Game"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.open_with),
+            tooltip: 'Moves',
+            onPressed: () { },
+          ),
+          Container(
+              padding: EdgeInsets.only(top: 18, right: 12),
+              child: Text(userScore.toString(), style: TextStyle(fontSize: 18),)
+          ),
+          IconButton(
+            icon: const Icon(Icons.watch_later),
+            tooltip: 'Time',
+            onPressed: () {
+              scaffoldKey.currentState.showSnackBar(snackBar);
+            },
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 18, right: 12),
+            child: Text(userScore.toString(), style: TextStyle(fontSize: 18),)
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
